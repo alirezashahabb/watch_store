b9b728024a2897564040cb427f8ab86a3bc0561e
alireza shahabadini <shehabalireza@gmail.com>
2024-07-01 16:28:54 +0330

    add Search Bar  to HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,43 @@
+import 'package:clock_shop/gen/assets.gen.dart';
+import 'package:clock_shop/res/color.dart';
+import 'package:clock_shop/res/dimends.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(
+                AppDimes.medium,
+              ),
+              decoration: BoxDecoration(
+                  color: AppColors.searchBar,
+                  borderRadius: BorderRadius.circular(60),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: AppColors.shadow,
+                      offset: Offset(0, 3),
+                      blurRadius: 10,
+                    )
+                  ]),
+              width: double.infinity,
+              height: 54,
+              child: Row(
+                children: [
+                  SvgPicture.asset(Assets.svg.search),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
