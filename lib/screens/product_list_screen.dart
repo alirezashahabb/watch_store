979573fb272e5b0a31a813b7e21c560c69c15338
alireza shahabadini <shehabalireza@gmail.com>
2024-07-21 11:05:20 +0330

    add cartItem to productList

--- lib/screens/product_list_screen.dart
+++ lib/screens/product_list_screen.dart
@@ -1,4 +1,7 @@
+import 'package:clock_shop/components/text_style.dart';
+import 'package:clock_shop/res/dimends.dart';
 import 'package:clock_shop/widget/custom_appbar.dart';
+import 'package:clock_shop/widget/product_item.dart';
 import 'package:flutter/material.dart';
 
 class ProductListScreen extends StatelessWidget {
@@ -8,6 +11,65 @@ class ProductListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: CustomAppBar(),
+      body: Column(
+        children: [
+          TagsTitle(),
+          GradViwProduct(),
+        ],
+      ),
+    );
+  }
+}
+
+class TagsTitle extends StatelessWidget {
+  const TagsTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: AppDimes.large),
+      child: SizedBox(
+        height: 32,
+        child: ListView.builder(
+          itemCount: 8,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Container(
+              alignment: Alignment.center,
+              margin: const EdgeInsets.symmetric(horizontal: AppDimes.medium),
+              padding: const EdgeInsets.symmetric(horizontal: AppDimes.medium),
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(AppDimes.large)),
+              child: const Text(
+                'تستی',
+                style: AppTextStyles.tagTitle,
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class GradViwProduct extends StatelessWidget {
+  const GradViwProduct({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 2,
+          mainAxisSpacing: 2,
+          childAspectRatio: 0.7,
+        ),
+        itemBuilder: (context, index) {
+          return const ProductItem(productName: 'ساعت هوشمند', price: 100000);
+        },
+      ),
     );
   }
 }
