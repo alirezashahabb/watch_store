13a7dbedc81e0725bdbfb08bc4b0f199780cf33a
alirezashahab <shehabalireza@gmail.com>
2025-02-28 08:42:39 -0800

    first commit

---
+++ lib/screens/send_otp_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SendOtpScreen extends StatelessWidget {
+  const SendOtpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return const Scaffold();
+  }
+}
