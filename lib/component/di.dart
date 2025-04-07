0caed8473e57469b11a7ead0347cdc27e2c566bd
alirezashahab <shehabalireza@gmail.com>
2025-04-07 12:49:48 -0700

    addProductDataSource

--- lib/component/di.dart
+++ lib/component/di.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:watch_store/constant/endpoinst.dart';
+import 'package:watch_store/data/model/data_source/product_data_source.dart';
 
 var locator = GetIt.instance;
 
@@ -11,7 +12,15 @@ Future<void> getItInit() async {
     await SharedPreferences.getInstance(),
   );
 
-  locator.registerSingleton<Dio>(Dio(BaseOptions(
-    baseUrl: Endpoints.baseUrl,
-  )));
+  locator.registerSingleton<Dio>(
+    Dio(
+      BaseOptions(
+        baseUrl: Endpoints.baseUrl,
+      ),
+    ),
+  );
+  //================================>>>>DataSource
+  locator.registerFactory<IProductDataSource>(
+    () => ProductDataSource(),
+  );
 }
---
+++ lib/data/model/data_source/product_data_source.dart
@@ -0,0 +1,78 @@
+import 'package:dio/dio.dart';
+import 'package:watch_store/component/di.dart';
+import 'package:watch_store/constant/endpoinst.dart';
+import 'package:watch_store/data/model/home_model.dart';
+import 'package:watch_store/utils/response_validator.dart';
+
+abstract class IProductDataSource {
+  Future<List<ProductModel>> getAllByCategory(int id);
+  Future<List<ProductModel>> getAllByBrand(int id);
+  Future<List<ProductModel>> getSoetrd(String routParam);
+  Future<List<ProductModel>> getAllProduct(String searchKey);
+}
+
+class ProductDataSource extends IProductDataSource {
+  final Dio httpClinet = locator.get();
+  @override
+  Future<List<ProductModel>> getAllByBrand(int id) async {
+    List<ProductModel> products = [];
+
+    Response response =
+        await httpClinet.get(Endpoints.productsByBrand + id.toString());
+
+    HTTPResponseValidator.isValidStatusCode(response.statusCode!);
+
+    for (var element in response.data['data'] as List) {
+      products.add(ProductModel.fromJson(element));
+    }
+
+    return products;
+  }
+
+  @override
+  Future<List<ProductModel>> getAllByCategory(int id) async {
+    List<ProductModel> products = [];
+
+    Response response =
+        await httpClinet.get(Endpoints.productsByCategory + id.toString());
+
+    HTTPResponseValidator.isValidStatusCode(response.statusCode!);
+
+    for (var element in response.data['data'] as List) {
+      products.add(ProductModel.fromJson(element));
+    }
+
+    return products;
+  }
+
+  @override
+  Future<List<ProductModel>> getAllProduct(String searchKey) async {
+    List<ProductModel> products = [];
+
+    Response response =
+        await httpClinet.get(Endpoints.baseUrl + searchKey.toString());
+
+    HTTPResponseValidator.isValidStatusCode(response.statusCode!);
+
+    for (var element in response.data['data'] as List) {
+      products.add(ProductModel.fromJson(element));
+    }
+
+    return products;
+  }
+
+  @override
+  Future<List<ProductModel>> getSoetrd(String routParam) async {
+    List<ProductModel> products = [];
+
+    Response response = await httpClinet.get(Endpoints.baseUrl + routParam);
+
+    HTTPResponseValidator.isValidStatusCode(response.statusCode!);
+
+    for (var element in response.data['data'] as List) {
+      products.add(ProductModel.fromJson(element));
+    }
+
+    return products;
+  }
+}
