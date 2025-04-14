71c90ed3e83de14d534880d02e6d1332eee41c50
alirezashahab <shehabalireza@gmail.com>
2025-04-14 08:52:37 -0700

    add ProdcutTitle

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:watch_store/component/extention.dart';
+import 'package:watch_store/component/text_style.dart';
+import 'package:watch_store/gen/assets.gen.dart';
+import 'package:watch_store/res/colors.dart';
 import 'package:watch_store/res/dimens.dart';
 import 'package:watch_store/screens/home/bloc/home_bloc.dart';
 import 'package:watch_store/widgets/app_slider.dart';
@@ -73,13 +76,88 @@ class _HomeScreenState extends State<HomeScreen> {
                               shrinkWrap: true,
                               itemBuilder: (context, index) {
                                 return Container(
+                                  padding: EdgeInsets.all(
+                                    AppDimens.small,
+                                  ),
                                   margin: EdgeInsets.only(
                                     left: AppDimens.large,
                                     right: AppDimens.medium,
                                   ),
                                   height: 298,
                                   width: 200,
-                                  color: Colors.black,
+                                  decoration: BoxDecoration(
+                                    borderRadius:
+                                        BorderRadiusDirectional.circular(
+                                            AppDimens.medium),
+                                    gradient: LinearGradient(
+                                      begin: Alignment.topCenter,
+                                      end: Alignment.bottomCenter,
+                                      colors: AppColors.productBgGradiant,
+                                    ),
+                                  ),
+                                  child: Column(
+                                    spacing: AppDimens.small,
+                                    children: [
+                                      Assets.png.unnamed.image(),
+                                      Align(
+                                        alignment: Alignment.centerRight,
+                                        child: Text(
+                                          'ساعت مردانه',
+                                          style: AppTextStyles.productTitle,
+                                        ),
+                                      ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        children: [
+                                          Container(
+                                            alignment: Alignment.center,
+                                            width: 50,
+                                            height: 25,
+                                            decoration: BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(
+                                                AppDimens.medium,
+                                              ),
+                                              color: AppColors.error,
+                                            ),
+                                            child: Text(
+                                              '20%',
+                                              style: AppTextStyles.mainbuttn,
+                                            ),
+                                          ),
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.end,
+                                            children: [
+                                              Text(
+                                                '500000 تومان',
+                                                style:
+                                                    AppTextStyles.productTitle,
+                                              ),
+                                              Text(
+                                                '250000',
+                                                style:
+                                                    AppTextStyles.oldPriceStyle,
+                                              ),
+                                              Container(
+                                                width: double.infinity,
+                                                height: 2,
+                                                color: AppColors
+                                                    .focusedBorderColor,
+                                              ),
+                                              Text(
+                                                '9:22:10',
+                                                style: AppTextStyles.title,
+                                              )
+                                            ],
+                                          )
+                                        ],
+                                      )
+                                    ],
+                                  ),
                                 );
                               },
                             ),
