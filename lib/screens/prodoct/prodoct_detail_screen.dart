79888549a1db4171e75260261adacdb8f93c2c97
alirezashahab <shehabalireza@gmail.com>
2025-05-06 08:31:14 -0700

    add Prodoct comment

--- lib/screens/prodoct/prodoct_detail_screen.dart
+++ lib/screens/prodoct/prodoct_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:watch_store/component/extention.dart';
 import 'package:watch_store/component/text_style.dart';
 import 'package:watch_store/gen/assets.gen.dart';
 import 'package:watch_store/res/colors.dart';
@@ -55,43 +56,65 @@ class _ProdoctDetailScreenState extends State<ProdoctDetailScreen> {
                 )
               ],
             )),
-            body: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                spacing: 8,
-                children: [
-                  ImageLoadingService(
-                    mainImage: state.productDetailes.image!,
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(
-                          AppDimens.small,
+            body: Stack(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      spacing: 8,
+                      children: [
+                        ImageLoadingService(
+                          mainImage: state.productDetailes.image!,
                         ),
-                        color: AppColors.searchBar,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        spacing: 8,
-                        children: [
-                          Text(
-                            'متن تستی',
-                            style: AppTextStyles.productTitle,
-                            textDirection: TextDirection.rtl,
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(
+                                AppDimens.small,
+                              ),
+                              color: AppColors.searchBar,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                spacing: 8,
+                                children: [
+                                  Text(
+                                    'متن تستی',
+                                    style: AppTextStyles.productTitle,
+                                    textDirection: TextDirection.rtl,
+                                  ),
+                                  Text(
+                                    'ای لشسکر صاحب زمان اماده باشس اماده باش !',
+                                    style: AppTextStyles.caption,
+                                    textDirection: TextDirection.rtl,
+                                  ),
+                                  Divider(),
+                                  ProdoctTabView(),
+                                  60.0.height,
+                                ],
+                              ),
+                            ),
                           ),
-                          Text(
-                            'ای لشسکر صاحب زمان اماده باشس اماده باش !',
-                            style: AppTextStyles.caption,
-                            textDirection: TextDirection.rtl,
-                          ),
-                        ],
-                      ),
+                        )
+                      ],
                     ),
-                  )
-                ],
-              ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  child: Container(
+                    color: Colors.blue,
+                    height: 60,
+                    width: double.infinity,
+                  ),
+                ),
+              ],
             ),
           );
         } else if (state is ProdoctDetailErrorState) {
@@ -109,5 +132,91 @@ class _ProdoctDetailScreenState extends State<ProdoctDetailScreen> {
         }
       },
     );
+  }
+}
+
+class ProdoctTabView extends StatefulWidget {
+  const ProdoctTabView({super.key});
+
+  @override
+  State<ProdoctTabView> createState() => _ProdoctTabViewState();
+}
+
+class _ProdoctTabViewState extends State<ProdoctTabView> {
+  var slectedTapIndex = 2;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: MediaQuery.sizeOf(context).width,
+          height: 50,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemExtent: MediaQuery.sizeOf(context).width / taps.length,
+            itemCount: taps.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() => slectedTapIndex = index);
+                },
+                child: Container(
+                  padding: EdgeInsets.all(
+                    AppDimens.medium,
+                  ),
+                  child: Text(
+                    taps[index],
+                    style: index == slectedTapIndex
+                        ? AppTextStyles.selectedTab
+                        : AppTextStyles.unSelectedTab,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+        IndexedStack(
+          index: slectedTapIndex,
+          children: [
+            ReviewProducts(),
+            FutureProducts(),
+            CommentsProdocts(),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+List<String> taps = [
+  'نقد برسی',
+  'نظرات',
+  'خصوصیات',
+];
+
+class FutureProducts extends StatelessWidget {
+  const FutureProducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class ReviewProducts extends StatelessWidget {
+  const ReviewProducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class CommentsProdocts extends StatelessWidget {
+  const CommentsProdocts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
